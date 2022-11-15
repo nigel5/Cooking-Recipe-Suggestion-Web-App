2e34beb6deaddd752c50b276ceb62ed354fae55e
Daniyil Kotov <46655615+kotovrdtrlf@users.noreply.github.com>
2022-11-14 23:27:49 -0500

    Recipes population script to include nutrition facts (#5)
    * Recipes population script to include nutrition facts
    
    * Uncommenting bulk creates
    
    * Model update

--- backend/model/Recipe.js
+++ backend/model/Recipe.js
@@ -1,52 +1,70 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Recipe', {
-    RecipeID: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true
-    },
-    Description: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    Cuisine: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ImgLink: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    Prep: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    CookTime: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    Servings: {
-      type: DataTypes.BIGINT,
-      allowNull: true
-    },
-    Name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'Recipe',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
-      {
-        name: "Recipe_pk",
-        unique: true,
-        fields: [
-          { name: "RecipeID" },
-        ]
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "Recipe",
+    {
+      RecipeID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
+      Description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Cuisine: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      ImgLink: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Prep: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      CookTime: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Servings: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+      },
+      Name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Calories: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Fat: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Carbs: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Protein: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: "Recipe",
+      schema: "public",
+      timestamps: false,
+      indexes: [
+        {
+          name: "Recipe_pk",
+          unique: true,
+          fields: [{ name: "RecipeID" }],
+        },
+      ],
+    }
+  );
 };
--- backend/util/importRecipes.js
+++ backend/util/importRecipes.js
@@ -2,32 +2,44 @@
  * Import recipes to dev
  */
 
-module.exports = function(recipeModel, recipeIngredientIndexModel, recipeStepModel, recipeIngredientModel) {
-    const { v4: uuidv4 } = require('uuid')
-    const fs = require('fs')
-    const getPrimaryKey = require('./getPrimaryKey')
-    const recipes = JSON.parse(fs.readFileSync('recipes.json', 'utf8'))
-    const ingredients = JSON.parse(fs.readFileSync('Ingredient_Index.json', 'utf8'))
+module.exports = function (
+    recipeModel,
+    recipeIngredientIndexModel,
+    recipeStepModel,
+    recipeIngredientModel
+) {
+    const { v4: uuidv4 } = require("uuid");
+    const fs = require("fs");
+    const getPrimaryKey = require("./getPrimaryKey");
+    const recipes = JSON.parse(fs.readFileSync("recipes.json", "utf8"));
+    const ingredients = JSON.parse(
+        fs.readFileSync("Ingredient_Index.json", "utf8")
+    );
 
-    const IngredientToIngredientID = {}
+    const IngredientToIngredientID = {};
     ingredients.forEach((ingredient) => {
-        IngredientToIngredientID[ingredient.IngredientString] = ingredient.IngredientID;
-    })
+        IngredientToIngredientID[ingredient.IngredientString] =
+            ingredient.IngredientID;
+    });
 
-    const bulkRecipes = []
-    const bulkRecipeSteps = []
-    const bulkRecipeIngredientIndex = []
-    const bulkRecipeIngredientStr = []
+    const bulkRecipes = [];
+    const bulkRecipeSteps = [];
+    const bulkRecipeIngredientIndex = [];
+    const bulkRecipeIngredientStr = [];
 
     recipes.forEach((recipe) => {
-        const name = recipe.name
-        const pk = getPrimaryKey(name)
-        const description = recipe["description"]
-        const cuisine = recipe["cuisine"]
-        const img  = recipe["img"]
-        const prepTime  = recipe["details"]["prep_time"]
-        const cookingTime  = recipe["details"]["cook_time"]
-        const servings  = recipe["details"]["servings"]
+        const name = recipe.name;
+        const pk = getPrimaryKey(name);
+        const description = recipe["description"];
+        const cuisine = recipe["cuisine"];
+        const img = recipe["img"];
+        const prepTime = recipe["details"]["prep_time"];
+        const cookingTime = recipe["details"]["cook_time"];
+        const servings = recipe["details"]["servings"];
+        const calories = recipe["nutrition_facts"]["calories"];
+        const fat = recipe["nutrition_facts"]["fat"];
+        const carbs = recipe["nutrition_facts"]["carbs"];
+        const protein = recipe["nutrition_facts"]["protein"];
 
         bulkRecipes.push({
             RecipeID: pk,
@@ -37,27 +49,31 @@ module.exports = function(recipeModel, recipeIngredientIndexModel, recipeStepMod
             ImgLink: img,
             Prep: prepTime,
             CookTime: cookingTime,
-            Servings: servings
-        })
+            Servings: servings,
+            Calories: calories,
+            Fat: fat,
+            Carbs: carbs,
+            Protein: protein,
+        });
 
         recipe.steps.forEach((step) => {
             bulkRecipeSteps.push({
                 StepID: uuidv4(),
                 OrderOfStep: Number(step.order),
                 StepInfo: step.description.trim(),
-                RecipeID: pk
-            })
-        })
-        
+                RecipeID: pk,
+            });
+        });
+
         // Associate this recipe with the ingredient for DISPLAY
-        const rawIngredientStr = recipe.ingredients_raw
+        const rawIngredientStr = recipe.ingredients_raw;
         rawIngredientStr.forEach((str) => {
             bulkRecipeIngredientStr.push({
                 RecipeID: pk,
                 RawIngredient: str,
-                RecipeIngredientID: uuidv4()
-            })
-        })
+                RecipeIngredientID: uuidv4(),
+            });
+        });
 
         // Associate this recipe with the ingredients for SEARCH
         const ingredients = recipe.ingredients_parsed;
@@ -65,36 +81,46 @@ module.exports = function(recipeModel, recipeIngredientIndexModel, recipeStepMod
             const ingredientPK = getPrimaryKey(ingredient);
             bulkRecipeIngredientIndex.push({
                 IngredientID: ingredientPK,
-                RecipeID: pk
-            })
+                RecipeID: pk,
+            });
+        });
+    });
+
+    recipeModel
+        .bulkCreate(bulkRecipes, {
+            ignoreDuplicates: true,
         })
-    })
+        .then(() => {
+            console.log(`Inserted/updated ${bulkRecipes.length} recipes`);
+        });
 
-    // recipeModel.bulkCreate(bulkRecipes, {
-    //     ignoreDuplicates: true
-    // })
-    // .then(() => {
-    //     console.log(`Inserted/updated ${bulkRecipes.length} recipes`);
-    // })
+    recipeStepModel
+        .bulkCreate(bulkRecipeSteps, {
+            ignoreDuplicates: true,
+        })
+        .then(() => {
+            console.log(
+                `Inserted/updated ${bulkRecipeSteps.length} recipe steps`
+            );
+        });
 
-    // recipeStepModel.bulkCreate(bulkRecipeSteps, {
-    //     ignoreDuplicates: true
-    // })
-    // .then(() => {
-    //     console.log(`Inserted/updated ${bulkRecipeSteps.length} recipe steps`);
-    // })
+    recipeIngredientModel
+        .bulkCreate(bulkRecipeIngredientStr, {
+            ignoreDuplicates: true,
+        })
+        .then(() => {
+            console.log(
+                `Inserted/updated ${bulkRecipes.length} recipe ingredients`
+            );
+        });
 
-    // recipeIngredientModel.bulkCreate(bulkRecipeIngredientStr, {
-    //     ignoreDuplicates: true
-    // })
-    // .then(() => {
-    //     console.log(`Inserted/updated ${bulkRecipes.length} recipe ingredients`);
-    // })
-
-    recipeIngredientIndexModel.bulkCreate(bulkRecipeIngredientIndex, {
-        ignoreDuplicates: true
-    })
-    .then(() => {
-        console.log(`Inserted/updated ${bulkRecipes.length} recipe to ingredients index`);
-    })
-}
+    recipeIngredientIndexModel
+        .bulkCreate(bulkRecipeIngredientIndex, {
+            ignoreDuplicates: true,
+        })
+        .then(() => {
+            console.log(
+                `Inserted/updated ${bulkRecipes.length} recipe to ingredients index`
+            );
+        });
+};
